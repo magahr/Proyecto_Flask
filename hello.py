9b9004eda3e1caeb64d611399048361ebbcba206
MagaDev <magahr123@gmail.com>
2024-08-01 20:01:50 -0400

    i don know

--- hello.py
+++ hello.py
@@ -8,27 +8,46 @@ app = Flask(__name__)
 
 students = ['Adrian', 'Hiram', 'Reykel', 'Magaly']
 
+students2 =[ 
+    {
+    'id':1,
+    'name':'luis',
+    'age':20
+    },
+     {
+    'id':2,
+    'name':'Maria',
+    'age':25
+    }
+]
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/oplesk")
 def social_oplesk():
-    return "<p>Hello, from oplesk!</p>"
+    return "<h1>Hello, from oplesk!</h1>"
 
 @app.route("/students", methods=['GET'])
 def get_students():
     return jsonify(students)
 
-@app.route("/create-students", methods=['POST'])
-def create_students():
+@app.route("/create-students2", methods=['POST'])
+def create_students2():
     data = request.json
-    students.append(data)
-    return jsonify( { 'message': 'Estudiante creado correctamente', 'Todos los estudiantes son : ': students})
+    students2.append(data)
+    return jsonify( { 'message': 'Estudiante creado correctamente', 'Todos los estudiantes son : ': students2})
     
-# @app.route("/delete-students", methods=['DELETE'])
-# def delete_students():
-#     data = request.json
-#     students.append(data)
-#     return jsonify( { 'message': 'Estudiante creado correctamente', 'Todos los estudiantes son : ': students})
-    
+   
+@app.route("/delete-students", methods=['DELETE'])
+def delete_all_students():
+    students.clear()
+    return jsonify( { 'message': 'Estudiantes borrado correctamente', 'Todos los estudiantes son : ': students})
+
+@app.route("/students/<int:student_id>", methods=['PATCH'])
+def get_students_by_id(student_id):
+    for student in students2:
+        if student.get('id') == student_id:
+            return jsonify(students2)
+    return jsonify( { 'message': 'Estudiantes no econtrado'})
